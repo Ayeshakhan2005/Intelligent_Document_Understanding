287fe2d0657e5b0da693eff73cd8a073b2ac20ce
Ayeshakhan2005 <ayeshaaikhan1@gmail.com>
2026-08-15 16:11:48 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,18 @@ import cv2
 import numpy as np
 
 st.title("Intelligent Document Understanding")
-
 uploaded_file = st.file_uploader("", type=["jpg","jpeg","png"])
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.image(image)
     
+    # BETTER PREPROCESSING FOR BAD PHOTOS
     img = np.array(image)
     img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+    img = cv2.dilate(img, np.ones((1,1), np.uint8), iterations=1)
     
     if st.button("Extract Text"):
-        text = pytesseract.image_to_string(img, lang='eng', config='--psm 6')
+        text = pytesseract.image_to_string(img, config='--psm 6')
         st.text_area("Extracted Text", text, height=400)
